1722df6c4f083ce04307e22f76bce5ce105918fb
Uthman Bakar <uthmanb@outlook.com>
2019-09-18 14:06:25 +0100

    disabled email sending function @ the create service call function in data/MySQLDatabase.php for local development purposes

--- data/MySQLDatabase.php
+++ data/MySQLDatabase.php
@@ -350,20 +350,20 @@ $message = "Dear engineer $engineerName,  \n<br> Please be informed that a servi
         'from'      => $email, // set from address here, it can really be anything
       );
 
-        curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-        $request =  $url.'api/mail.send.json';
-        // Generate curl request
-        $session = curl_init($request);
-        // Tell curl to use HTTP POST
-        curl_setopt ($session, CURLOPT_POST, true);
-        // Tell curl that this is the body of the POST
-        curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-        // Tell curl not to return headers, but do return the response
-        curl_setopt($session, CURLOPT_HEADER, false);
-        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-        // obtain response
-        $response = curl_exec($session);
-        curl_close($session);
+//        curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+//        $request =  $url.'api/mail.send.json';
+//        // Generate curl request
+//        $session = curl_init($request);
+//        // Tell curl to use HTTP POST
+//        curl_setopt ($session, CURLOPT_POST, true);
+//        // Tell curl that this is the body of the POST
+//        curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+//        // Tell curl not to return headers, but do return the response
+//        curl_setopt($session, CURLOPT_HEADER, false);
+//        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+//        // obtain response
+//        $response = curl_exec($session);
+//        curl_close($session);
         // Redirect to thank you page upon successfull completion, will want to build one if you don't alreday have one available
       
   $this->createActivityNotifications($message,$machineID,$accountID);
