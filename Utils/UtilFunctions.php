ef9d62e73b1839c2d7bdf8e01239a3b3ac828d7e
Uthman Bakar <uthmanb@outlook.com>
2019-09-20 14:55:06 +0100

    Added new class file Util/UtilFunctions.php, file will hold utility functions

--- Utils/UtilFunctions.php
+++ Utils/UtilFunctions.php
@@ -8,5 +8,20 @@
 
 class UtilFunctions
 {
+    public static function textSummary($string){
+        $string = strip_tags($string);
+        $length = 150;
 
+        if (strlen($string) > $length) {
+
+            // truncate string
+            $stringCut = substr($string, 0, $length);
+            $endPoint = strrpos($stringCut, ' ');
+
+            //if the string doesn't contain any space then it will cut without word basis.
+            $string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+            $string .= ' ... ';
+        }
+        return $string;
+    }
 }
